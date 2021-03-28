d7b0dabe81580256f1647c5af291bfb24f934835
André Soares Nascimento <andresoaresnascimento@gmail.com>
2021-03-27 23:41:12 -0300

    Change RequestMapping to GetMapping

--- src/main/java/com/helloworld/demo/HelloWorldController.java
+++ src/main/java/com/helloworld/demo/HelloWorldController.java
@@ -1,16 +1,15 @@
 package com.helloworld.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping(path = "/")
     @ResponseBody
     public String helloworld() {
-        return "Hello World!!!";
+        return "Hello World!!?!";
     }
 }
